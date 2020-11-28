9658911ed1234e5cc88fd208844082bbeaa1b263
Crabislav <alex.kopnin2@gmail.com>
2020-11-28 17:49:16 +0200

    add validateValues method to Utils.java

--- src/main/java/task1/Utils.java
+++ src/main/java/task1/Utils.java
@@ -1,6 +1,7 @@
 package task1;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import task1.exceptions.BadRequestException;
 
 public class Utils {
     static final String RULES_PATH = "conversionRules.json";
@@ -13,5 +14,17 @@ public class Utils {
         return mapper;
     }
 
+    static void validateValues(String from, String to, double value) throws BadRequestException {
+        if (from == null || from.isEmpty()) {
+            throw new BadRequestException("\"From\" can't be null or empty. Your input is: " + from);
+        }
 
+        if (to == null || to.isEmpty()) {
+            throw new BadRequestException("\"To\" can't be null or empty. Your input is: " + to);
+        }
+
+        if (value <= 0) {
+            throw new BadRequestException("\"Value\" must be greater than 0. Your input is: " + value);
+        }
+    }
 }
