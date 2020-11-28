e9e5875b8596b98e3a9de1cd5d2f9a7b5f7a4795
Crabislav <alex.kopnin2@gmail.com>
2020-11-28 17:53:05 +0200

    add ConverterTest.java

---
+++ src/test/java/ConverterTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import task1.Converter;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConverterTest {
+    @Test
+    public void convertFromOneMeterToCmShouldReturn100() throws Exception {
+        String testValue = "{\"distance\": {\"unit\": \"m\", \"value\": 1}, \"convert_to\": \"cm\"}";
+        String expectedResult = "{\"unit\":\"cm\",\"value\":100.0}";
+        assertEquals(expectedResult, Converter.convert(testValue));
+    }
+}
