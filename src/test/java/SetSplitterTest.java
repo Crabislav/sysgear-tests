947b05cb2a2ae4c7fe8f57e1a3baea13e5b95838
Crabislav <alex.kopnin2@gmail.com>
2020-11-29 21:44:50 +0200

    add new test to SetSplitterTest.java

--- src/test/java/SetSplitterTest.java
+++ src/test/java/SetSplitterTest.java
@@ -8,9 +8,16 @@ import static org.junit.Assert.assertEquals;
 public class SetSplitterTest {
 
     @Test
-    public void getSetsShouldReturnTwoBalancedArrays() throws IOException {
+    public void getSetsShouldReturnTwoBalancedArrays1() throws IOException {
         String testValue = "{\"set\": [4,5,6,7,8]}";
         String expectedResult = "{\"set_1\":[4,5,6],\"set_2\":[7,8]}";
+        assertEquals(expectedResult, SetSplitter.getSets(testValue).toString());
+    }
+
+    @Test
+    public void getSetsShouldReturnTwoBalancedArrays2() throws IOException {
+        String testValue = "{\"set\": [3,3,3,7,5]}";
+        String expectedResult = "{\"set_1\":[3,3,5],\"set_2\":[3,7]}";
         assertEquals(expectedResult, SetSplitter.getSets(testValue).toString());
     }
 }
