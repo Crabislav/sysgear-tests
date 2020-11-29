9649c7ce6aabfe26f05a667d6eacf14720b2cbab
Crabislav <alex.kopnin2@gmail.com>
2020-11-29 21:46:57 +0200

    code refactor Utils.java

--- src/main/java/task3/Utils.java
+++ src/main/java/task3/Utils.java
@@ -1,12 +1,7 @@
 package task3;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
-import java.io.IOException;
 
 public class Utils {
     private static ObjectMapper mapper = getObjectMapperInstance();
@@ -18,31 +13,7 @@ public class Utils {
         return mapper;
     }
 
-    public static JsonNode parse(String src) throws IOException {
-        return mapper.readTree(src);
-    }
-
-    public static <T> T fromJson(JsonNode node, Class<T> clazz) throws JsonProcessingException {
-        return mapper.treeToValue(node, clazz);
-    }
-
     public static JsonNode toJson(Object obj) {
         return mapper.valueToTree(obj);
-    }
-
-    public static String stringify(JsonNode node) throws JsonProcessingException {
-        return generateString(node, false);
-    }
-
-    public static String prettyPrint(JsonNode node) throws JsonProcessingException {
-        return generateString(node, true);
-    }
-
-    private static String generateString(JsonNode node, boolean pretty) throws JsonProcessingException {
-        ObjectWriter objectWriter = mapper.writer();
-        if (pretty) {
-            objectWriter = objectWriter.with(SerializationFeature.INDENT_OUTPUT);
-        }
-        return objectWriter.writeValueAsString(node);
     }
 }
