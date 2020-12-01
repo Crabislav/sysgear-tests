1a1d78757b884654d30a0862dd56743b60304f33
Crabislav <alex.kopnin2@gmail.com>
2020-12-02 00:34:51 +0200

    add BadRequestException.java, RuleNotFoundException.java

---
+++ src/main/java/task2/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package task2.exceptions;
+
+public class BadRequestException extends Exception {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/task2/exceptions/RuleNotFoundException.java
@@ -0,0 +1,7 @@
+package task2.exceptions;
+
+public class RuleNotFoundException extends Exception {
+    public RuleNotFoundException(String message) {
+        super(message);
+    }
+}
