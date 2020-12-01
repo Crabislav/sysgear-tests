d24d93b17400b9a807f62ae60e366863df634d4a
Crabislav <alex.kopnin2@gmail.com>
2020-12-01 21:16:01 +0200

    code refactor

--- src/main/java/task3/SetSplitter.java
+++ src/main/java/task3/SetSplitter.java
@@ -3,13 +3,9 @@ package task3;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import javafx.util.Pair;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class SetSplitter {
     public static void main(String[] args) {
@@ -20,20 +16,20 @@ public class SetSplitter {
         JsonNode setValuesNode = Utils.getObjectMapperInstance().readTree(src).get("set");
         List<Integer> values = Utils.getObjectMapperInstance().treeToValue(setValuesNode, List.class);
 
-        Pair<List<Integer>, List<Integer>> sets = findSets(values);
-
-        JsonNode set1 = Utils.toJson(sets.getValue());
-        JsonNode set2 = Utils.toJson(sets.getKey());
+        List<List<Integer>> sets = findSets(values);
 
         JsonNodeFactory factory = new JsonNodeFactory(false);
         ObjectNode result = factory.objectNode();
-        result.set("set_1", set1);
-        result.set("set_2", set2);
+
+        for (int i = 0; i < sets.size(); i++) {
+            JsonNode set = Utils.getObjectMapperInstance().valueToTree(sets.get(i));
+            result.set("set_" + (i + 1), set);
+        }
 
         return result;
     }
 
-    private static Pair<List<Integer>, List<Integer>> findSets(List<Integer> inputSet) {
+    private static List<List<Integer>> findSets(List<Integer> inputSet) {
         int tempSum = 0;
         int weight = getSum(inputSet) / 2;
 
@@ -44,9 +40,9 @@ public class SetSplitter {
 
         for (Integer currValue : inputSet) {
             if (tempSum + currValue > weight) {
-                set2.add(currValue);
-            } else {
                 set1.add(currValue);
+            } else {
+                set2.add(currValue);
                 tempSum += currValue;
             }
         }
@@ -54,7 +50,7 @@ public class SetSplitter {
         Collections.sort(set1);
         Collections.sort(set2);
 
-        return new Pair<>(set1, set2);
+        return new ArrayList<>(Arrays.asList(set1, set2));
     }
 
     private static int getSum(List<Integer> input) {
