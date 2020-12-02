b676384b8189656dac4dcb2eb0eaf8d37c175c29
Crabislav <alex.kopnin2@gmail.com>
2020-12-02 13:59:30 +0200

    remove redundant field and methods at Rule.java

--- src/main/java/task2/rules/Rule.java
+++ src/main/java/task2/rules/Rule.java
@@ -5,38 +5,22 @@ import task2.exceptions.RuleNotFoundException;
 import java.util.List;
 
 public abstract class Rule {
-    private String name;
     private Object value;
 
     public static Rule getRuleInstance(String ruleName) throws RuleNotFoundException {
-        Rule rule;
         switch (ruleName) {
             case "include":
-                rule = new Include();
-                break;
+                return new Include();
             case "sort_by":
-                rule = new SortBy();
-                break;
+                return new SortBy();
             case "exclude":
-                rule = new Exclude();
-                break;
+                return new Exclude();
             default:
                 throw new RuleNotFoundException("Rule " + ruleName + " was not found");
         }
-
-        rule.setName(ruleName);
-        return rule;
     }
 
     public abstract void applyRule(List<Object> rawData);
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public Object getValue() {
         return value;
@@ -49,8 +33,7 @@ public abstract class Rule {
     @Override
     public String toString() {
         return "Rule{" +
-                "name='" + name + '\'' +
-                ", value=" + value +
+                "value=" + value +
                 '}';
     }
 }
