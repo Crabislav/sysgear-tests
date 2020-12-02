90c072feebe98acf2b078d38179c6b92e2a152e1
Crabislav <alex.kopnin2@gmail.com>
2020-12-02 15:05:38 +0200

    add custom exceptions

--- src/main/java/task2/rules/SortBy.java
+++ src/main/java/task2/rules/SortBy.java
@@ -1,11 +1,15 @@
 package task2.rules;
 
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class SortBy extends Rule {
-
     @Override
-    public void applyRule(List<Object> rawData) {
-
+    public void applyRule(List<LinkedHashMap<String, String>> rawData) {
+        List<String> sortingKeys = (List<String>) getValue();
+        for (String key : sortingKeys) {
+            rawData.sort(Comparator.comparing((LinkedHashMap<String, String> a) -> a.get(key)));
+        }
     }
 }
